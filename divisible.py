4a5776f118bf14150318bc3eb321126e5fed1433
przemyslawwalerczyk <przemyslawwalerczyk@gmail.com>
2023-05-06 11:34:05 +0200

    Create divisible.py

---
+++ divisible.py
@@ -0,0 +1,13 @@
+for i in range(1, 101):
+  if i % 3 == 0:
+    print(i)
+    
+#b)
+for i in range(1, 101):
+  if i % 7 == 0:
+    print(i)
+
+#c)
+for i in range(1, 101):
+  if i % 3 == 0 and i % 7 == 0:
+    print(i)
